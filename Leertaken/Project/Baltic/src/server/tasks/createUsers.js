1072eae8ebdc896b4f38b5192bfcb00b0a8cd3f9
Andre Nanninga <ag.nanninga@gmail.com>
2014-06-24 15:46:46 +0200

    added extra default user

--- Leertaken/Project/Baltic/src/server/tasks/createUsers.js
+++ Leertaken/Project/Baltic/src/server/tasks/createUsers.js
@@ -2,11 +2,19 @@ Meteor.startup(function() {
 	var users = app.collections.users.find().fetch();
 
 	if(users.length === 0) {
+		console.log('Creating initial users.');
+
 		var users = [
 			{
 				username: 'admin',
 				password: 'admin',
 				email: 'admin@davincidata.nl',
+				profile: {}
+			},
+			{
+				username: 'user',
+				password: 'password',
+				email: 'user@davincidata.nl',
 				profile: {}
 			}
 		];
