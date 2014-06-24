5aa6933703a920433b2769422e60022f1cf4555d
mauvm <maurits@nerdieworks.nl>
2014-06-24 11:44:19 +0200

    Add temperature and humidity graphs.

--- Leertaken/Project/Baltic/src/client/helpers/graph.js
+++ Leertaken/Project/Baltic/src/client/helpers/graph.js
@@ -1,13 +1,29 @@
 var Graph = {};
 
+Graph.getStartDate = function() {
+	return new Date() - 24 * 60 * 60 * 1000;
+};
+
+Graph.getStopDate = function() {
+	return new Date();
+};
+
 Graph.setStations = function( stations ) {
+	var self = this;
+
 	if( app.subscriptions.graphMeasurements ) {
 		app.subscriptions.graphMeasurements.stop();
 	}
 
-	app.subscriptions.graphMeasurements = Meteor.subscribe( 'measurementAverages', [ this.stn ], new Date() - 10 * 60 * 1000 );
+	app.subscriptions.graphMeasurements = Meteor.subscribe( 'measurementAverages', stations, this.getStartDate(), this.getStopDate(), function() {
+		self.drawGraphs();
+	} );
+};
 
-	this.drawGraphs();
+Graph.getMeasurements = function() {
+	var measurements = app.collections.measurementAverages.find( {}, { sort: { datetime: 1 } } ).fetch();
+
+	return _.groupBy( measurements, function( m ){ return m.stn; } );
 };
 
 Graph.getTemperatureGraph = function() {
@@ -24,25 +40,157 @@ Graph.getHumidityGraph = function() {
 	return this._$graphHumi;
 };
 
-Graph.drawGraphs = function() {
+Graph._drawTemperatureGraph = function( measurements ) {
 	var $temp = this.getTemperatureGraph();
-	var $humi = this.getHumidityGraph();
 
 	$temp.empty();
+
+	var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+		width  = 600 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
+
+	var x = d3.time.scale()
+		.range( [ 0, width ] )
+		;
+
+	var y = d3.scale.linear()
+		.range( [ height, 0 ] )
+		;
+
+	var xAxis = d3.svg.axis()
+		.scale( x )
+		.orient( 'bottom' )
+		;
+
+	var yAxis = d3.svg.axis()
+		.scale( y )
+		.orient( 'left' )
+		;
+
+	var line = d3.svg.line()
+		.x( function( d ) { return x( d.datetime ); } )
+		.y( function( d ) { return y( d.avg_temp || 0 ); } )
+		;
+
+	var svg = d3.select( $temp[0] ).append( 'svg' )
+		.attr( 'width', width + margin.left + margin.right )
+		.attr( 'height', height + margin.top + margin.bottom )
+		.append( 'g' )
+		.attr( 'transform', 'translate(' + margin.left + ',' + margin.top + ')' )
+		;
+
+	var color = d3.scale.category20();
+	var coloring = function( d, i ) { return color(i); };
+
+	x.domain( [ this.getStartDate(), this.getStopDate() ] );
+	y.domain( [ -40, 60 ] );
+
+	svg.append( 'g' )
+		.attr( 'class', 'x axis')
+		.attr( 'transform', 'translate(0,' + height + ')' )
+		.call( xAxis )
+		;
+
+	svg.append( 'g' )
+		.attr( 'class', 'y axis' )
+		.call( yAxis )
+		.append( 'text' )
+		.attr( 'transform', 'rotate( -90 )' )
+		.attr( 'y', 6 )
+		.attr( 'dy', '.71em' )
+		.style( 'text-anchor', 'end' )
+		.text( 'Temperature (C°)' )
+		;
+
+	for( var stn in measurements ) {
+		svg.append( 'path' )
+			.datum( measurements[ stn ] )
+			.attr( 'd', line )
+			.attr( 'class', 'line' )
+			.style( 'stroke', coloring )
+			;
+	}
+};
+
+Graph._drawHumidityGraph = function( measurements ) {
+	var $humi = this.getHumidityGraph();
+
 	$humi.empty();
 
-	var data = [4, 8, 15, 16, 23, 42];
+	var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+		width  = 600 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
 
-	var x = d3.scale.linear()
-		.domain([0, d3.max(data)])
-		.range([0, 420]);
+	var x = d3.time.scale()
+		.range( [ 0, width ] )
+		;
 
-	d3.select($temp[0])
-		.selectAll("div")
-		.data(data)
-		.enter().append("div")
-		.style("width", function(d) { return x(d) + "px"; })
-		.text(function(d) { return d; });
+	var y = d3.scale.linear()
+		.range( [ height, 0 ] )
+		;
+
+	var xAxis = d3.svg.axis()
+		.scale( x )
+		.orient( 'bottom' )
+		;
+
+	var yAxis = d3.svg.axis()
+		.scale( y )
+		.orient( 'left' )
+		;
+
+	var line = d3.svg.line()
+		.x( function( d ) { return x( d.datetime ); } )
+		.y( function( d ) { return y( d.avg_humi || 0 ); } )
+		;
+
+	var svg = d3.select( $humi[0] ).append( 'svg' )
+		.attr( 'width', width + margin.left + margin.right )
+		.attr( 'height', height + margin.top + margin.bottom )
+		.append( 'g' )
+		.attr( 'transform', 'translate(' + margin.left + ',' + margin.top + ')' )
+		;
+
+	var color = d3.scale.category20();
+	var coloring = function( d, i ) { return color(i); };
+
+	x.domain( [ this.getStartDate(), this.getStopDate() ] );
+	y.domain( [ 0, 100 ] );
+
+	svg.append( 'g' )
+		.attr( 'class', 'x axis')
+		.attr( 'transform', 'translate(0,' + height + ')' )
+		.call( xAxis )
+		;
+
+	svg.append( 'g' )
+		.attr( 'class', 'y axis' )
+		.call( yAxis )
+		.append( 'text' )
+		.attr( 'transform', 'rotate( -90 )' )
+		.attr( 'y', 6 )
+		.attr( 'dy', '.71em' )
+		.style( 'text-anchor', 'end' )
+		.text( 'Humidity (%)' )
+		;
+
+	for( var stn in measurements ) {
+		svg.append( 'path' )
+			.datum( measurements[ stn ] )
+			.attr( 'd', line )
+			.attr( 'class', 'line' )
+			.style( 'stroke', coloring )
+			;
+	}
+};
+
+Graph.drawGraphs = function() {
+	var measurements = this.getMeasurements();
+
+	// console.log(measurements);
+
+	this._drawTemperatureGraph( measurements );
+	this._drawHumidityGraph( measurements );
 };
 
 Template.graph.events = {
