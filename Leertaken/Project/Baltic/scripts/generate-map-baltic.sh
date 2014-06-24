4ead8ff9fc5040bb69a36f9a01e34c328761dcf9
Andre Nanninga <ag.nanninga@gmail.com>
2014-06-24 15:43:21 +0200

    Added task to generate a GeoJson file of the world.

--- Leertaken/Project/Baltic/scripts/generate-map-baltic.sh
+++ Leertaken/Project/Baltic/scripts/generate-map-baltic.sh
@@ -1,10 +1,12 @@
 ogr2ogr \
+	-simplify .001 \
 	-f GeoJSON \
 	-where "ADM0_A3_IS IN ('SWE', 'DNK', 'DEU', 'POL', 'RUS', 'LTU', 'LVA', 'EST', 'FIN', 'NOR', 'NLD', 'BLR')" \
 	/vagrant/temp/countries.json \
 	/vagrant/resources/ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp
 
 ogr2ogr \
+	-simplify .1 \
 	-f GeoJSON \
 	-where "id IN ('1a', '1b', '1c', '1', '2')" \
 	/vagrant/temp/seas.json \
@@ -24,3 +26,14 @@ topojson \
 	-o /vagrant/src/public/geo/baltic.json \
 	-p name \
 	-- /vagrant/temp/topo_countries.json /vagrant/temp/topo_seas.json
+
+ogr2ogr \
+	-simplify .05 \
+	-f GeoJSON \
+	/vagrant/temp/world.json \
+	/vagrant/resources/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp
+
+topojson \
+	-o /vagrant/src/public/geo/world.json \
+	-p name=NAME \
+	-- /vagrant/temp/world.json
